a434584baf7e7a7b84238dd64d4cb156b79b5b9a
Fadil4320 <114566547+Fadil4320@users.noreply.github.com>
2023-03-01 11:33:17 +0700

    Add files via upload

---
+++ week1/test.php
@@ -0,0 +1,16 @@
+<?php
+// ============================================================
+// definisikan variable
+$nama = 'Rizky';
+$umur = 19;
+$berat = 65;
+
+echo 'Nama : ' .$nama;
+echo '<br/>Umur : ' .$umur. ' Tahun ';
+echo '<br/>Berat : ' .$berat. ' kg ';
+
+echo "<hr>";
+
+echo "<br/> Hello $nama ApaKabar?";
+echo "<br/> Hello Kabar saya baik";
+?>
